156e3d28ea3776b8f34ec705cae6c3d3f47f8f8c
Joan Parra <115508888+parra-aten@users.noreply.github.com>
2022-12-06 22:41:50 -0600

    Update itemBD.php

--- keramika/php/itemBD.php
+++ keramika/php/itemBD.php
@@ -8,11 +8,6 @@
 
     $pic1=$_FILES['itemPic1'];
     $filename1=$pic1['name'];
-    $temp=$pic1['tmp_name'];
-    $ruta='../img/pots/'.$filename1;
-    move_uploaded_file($temp,$ruta);
-
-
     move_uploaded_file($pic1['tmp_name'],'../img/pots/'.$name.'/'.$filename1);
     $foto1='img/pots/'.$name.'/'.$filename1;
 
