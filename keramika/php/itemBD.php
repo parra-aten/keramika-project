0a6c70472f7a308eb3b324377562e9d49c24d78c
Joan Parra <115508888+parra-aten@users.noreply.github.com>
2022-12-07 15:49:11 -0600

    Update itemBD.php

--- keramika/php/itemBD.php
+++ keramika/php/itemBD.php
@@ -5,23 +5,25 @@
     $style = $_POST["itemStyle"];
     $color = $_POST["itemColor"];
     $size = $_POST["itemSize"];
+    
+    mkdir('../img/pots/'.$name);
 
     $pic1=$_FILES['itemPic1'];
     $filename1=$pic1['name'];
-    $pic_one='img/pots/'.$name.'/'.$filename1;
-    move_uploaded_file($pic1['tmp_name'],'..'.$pic_one);
+    $pic_one='./img/pots/'.$name.'/'.$filename1;
+    move_uploaded_file($pic1['tmp_name'],'.'.$pic_one);
     
 
     $pic2=$_FILES['itemPic2'];
     $filename2=$pic2['name'];
-    $pic_two='img/pots/'.$name.'/'.$filename2;
-    move_uploaded_file($pic2['tmp_name'],'..'.$pic_two);
+    $pic_two='./img/pots/'.$name.'/'.$filename2;
+    move_uploaded_file($pic2['tmp_name'],'.'.$pic_two);
     
 
     $pic3=$_FILES['itemPic3'];
     $filename3=$pic3['name'];
-    $pic_three='img/pots/'.$name.'/'.$filename3;
-    move_uploaded_file($pic3['tmp_name'],'..'.$pic_three);
+    $pic_three='./img/pots/'.$name.'/'.$filename3;
+    move_uploaded_file($pic3['tmp_name'],'.'.$pic_three);
 
     if($name == "" || $style == "" || $color == ""||$size == ""){
         echo '<h1>Please fill in all the fields</h1>';
